0a823c5908347a5d2369fbd965954152e71e3620
Aqsa Jimmy <aqsa.jimmy24@gmail.com>
2025-06-06 03:02:06 +0700

    update:dashboard view

--- src/views/auth/Dashboard.vue
+++ src/views/auth/Dashboard.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import axiosInstance from "@/lib/axios";
+import { get } from "http";
 import { onMounted, onUnmounted, reactive, ref } from "vue";
 
 // interface DashboardData {
@@ -39,26 +40,28 @@ const logout = async () => {
       name: "",
       email: "",
     };
+    //clear cookie after logout
+    document.cookie = "XSRF-TOKEN=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+    document.cookie = "laravel_session=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+    window.location.href = "/login";
   } catch (error: any) {
     errors.value = error.response?.data?.errors || ["An error occurred"];
     console.error(error.response?.data);
   }
 };
-
-onMounted(() => {
-  getUser();
-});
+getUser();
 </script>
 <template>
   <div class="max-w-2xl mx-auto mt-10">
     <h1>Dashboard</h1>
   </div>
-  <div v-if="user.name">
+  <div v-if="user.name" class="max-w-2xl mx-auto mt-10 flex flex-col items-center bg-white p-6 rounded-lg shadow-md">
+    <h2 class="text-2xl font-bold mb-4">User Information</h2>
     <p>Welcome, {{ user.name }}!</p>
     <p>Email: {{ user.email }}</p>
     <button
       @click="logout"
-      class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+      class="text-white bg-slate-800 border-0 py-2 px-6 focus:outline-none hover:bg-slate-700 rounded text-lg focus:shadow-outline cursor-pointer mt-4"
     >
       Logout
     </button>
